1cc7269488cf4d7e51fb6532985d3eae113690ca
usman-hameed130 <150533814+usman-hameed130@users.noreply.github.com>
2025-09-21 10:26:05 +0000

    variable

---
+++ 1_j.js
@@ -0,0 +1,8 @@
+// console.log("usman");
+const accountID = 1
+let accountEmail  = "usman@gmail.com"
+var accountPassword = "123456"
+// console.log(accountEmail);
+city = "peshawar"
+let state
+console.table([accountID,accountEmail,accountPassword,state,city])
